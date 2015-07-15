2600fa49635dd07b5e24e7f2dc55a537d6945901
Ashleigh Maund <ashleighmaund@gmail.com>
2015-07-15 19:14:10 +0100

    page is showing a list of books

--- mainCtrl.js
+++ mainCtrl.js
@@ -2,5 +2,22 @@ angular.module('app').controller("MainController", function() {
   var self = this;
 
   self.title = "Yo";
-  
+  self.searchInput = '';
+  self.books = [
+    {
+      title: 'Game of Thrones',
+      year: 2011,
+      favorite: false
+    },
+    {
+      title: 'Harry Potter',
+      year: 2002,
+      favorite: true
+    },
+    {
+      title: 'Mortal Instruments',
+      year: 2008,
+      favorite: true
+    }
+  ];
 });
