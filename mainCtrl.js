dbe30b9c891e9d89acfa5592090ff3a00e1ad66d
Ashleigh Maund <ashleighmaund@gmail.com>
2015-07-15 19:25:41 +0100

    added a star next to favourited books

--- mainCtrl.js
+++ mainCtrl.js
@@ -2,22 +2,25 @@ angular.module('app').controller("MainController", function() {
   var self = this;
 
   self.title = "Yo";
+
   self.searchInput = '';
+
   self.books = [
     {
       title: 'Game of Thrones',
       year: 2011,
-      favorite: false
+      favourite: false
     },
     {
       title: 'Harry Potter',
       year: 2002,
-      favorite: true
+      favourite: true
     },
     {
       title: 'Mortal Instruments',
       year: 2008,
-      favorite: true
+      favourite: true
     }
   ];
+
 });
